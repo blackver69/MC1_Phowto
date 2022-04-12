ac04d334c98bd62f6e33356c942216b642ddb4cc
blackver69 <Rickychen930@gmail.com>
2022-04-12 17:49:53 +0700

    Initial Commit

---
+++ MC1_Phowto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MC1_Phowto
+//
+//  Created by Ricky on 12/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
